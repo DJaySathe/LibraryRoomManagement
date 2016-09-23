824b3ea05f1f4228612deb0f966bc271b14d81a7
Mahesh Masale <masale123@hotmail.com>
2016-09-23 18:52:32 -0400

    Added a Rooms table and functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923222459) do
+ActiveRecord::Schema.define(version: 20160923224542) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer  "roomid"
+    t.boolean  "status"
+    t.integer  "size"
+    t.string   "building"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
